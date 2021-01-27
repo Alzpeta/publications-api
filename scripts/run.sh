11dd682d888966454513dc290056bc42aee8b452
Miroslav Bauer <mirekys@users.noreply.github.com>
2021-01-27 15:27:28 +0100

    update run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd `dirname $0`
-
-export FLASK_ENV=development 
+export FLASK_ENV=development
 
 invenio run --cert development/server.crt --key development/server.key --port 8080 --host 127.0.0.1
