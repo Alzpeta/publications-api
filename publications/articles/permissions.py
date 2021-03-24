1418735a54cdedba55dbbb69c8d9bdd178521adb
Miroslav Bauer <mirekys@users.noreply.github.com>
2021-03-24 10:09:39 +0100

    typo fix

--- publications/articles/permissions.py
+++ publications/articles/permissions.py
@@ -5,3 +5,26 @@
 # CESNET OA Publication Repository is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 #
+from invenio_records_rest.utils import deny_all
+from oarepo_communities.permissions import create_object_permission_impl, read_object_permission_impl, \
+    delete_object_permission_impl, publish_permission_impl, unpublish_permission_impl, update_object_permission_impl
+
+from publications.permissions import ADMIN_ROLE_PERMISSIONS
+
+create_draft_object_permission_impl = create_object_permission_impl
+update_draft_object_permission_impl = update_object_permission_impl
+read_draft_object_permission_impl = read_object_permission_impl
+delete_draft_object_permission_impl = delete_object_permission_impl
+list_draft_object_permission_impl = deny_all
+
+# DRAFT dataset file manipulation
+put_draft_file_permission_impl = update_object_permission_impl
+delete_draft_file_permission_impl = update_object_permission_impl
+get_draft_file_permission_impl = read_draft_object_permission_impl
+
+# DRAFT dataset publishing
+publish_draft_object_permission_impl = publish_permission_impl
+unpublish_draft_object_permission_impl = unpublish_permission_impl
+
+# PUBLISHED dataset manipulation
+update_object_permission_impl = ADMIN_ROLE_PERMISSIONS
