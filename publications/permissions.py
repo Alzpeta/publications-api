8092cf6bcf775e3d4e89ec53e309ea831aa75b9e
Miroslav Bauer <mirekys@users.noreply.github.com>
2021-03-25 18:10:11 +0100

    implement dataset search permissions filters

--- publications/permissions.py
+++ publications/permissions.py
@@ -7,6 +7,7 @@
 #
 from flask_principal import Permission, RoleNeed
 from invenio_access import authenticated_user
+from oarepo_communities.permissions import community_member_permission_impl, community_curator_permission_impl
 
 CURATOR_ROLE_PERMISSIONS = Permission(
     RoleNeed('curator')
@@ -21,3 +22,6 @@ ADMIN_ROLE_PERMISSIONS = Permission(
 )
 
 AUTHENTICATED_PERMISSION = Permission(authenticated_user)
+
+COMMUNITY_MEMBER_PERMISSION = community_member_permission_impl
+COMMUNITY_CURATOR_PERMISSION = community_curator_permission_impl
